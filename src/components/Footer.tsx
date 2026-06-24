22963c5b276a274368169047d53642b03c04da64
saudemaisfit-beep <saudemaisfit@gmail.com>
2026-06-24 15:09:26 +0100

    feat: replace Facebook text icon with facebook.png in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -45,6 +45,8 @@ export default function Footer() {
                     <Image src="/linkedin.png" alt="LinkedIn" width={38} height={38} style={{ objectFit: 'cover', display: 'block' }} />
                   ) : s.label === 'Instagram' ? (
                     <Image src="/instagram.png" alt="Instagram" width={38} height={38} style={{ objectFit: 'cover', display: 'block' }} />
+                  ) : s.label === 'Facebook' ? (
+                    <Image src="/facebook.png" alt="Facebook" width={38} height={38} style={{ objectFit: 'cover', display: 'block' }} />
                   ) : s.icon}
                 </a>
               ))}
