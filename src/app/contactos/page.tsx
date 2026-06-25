08391198f93dee182ebe4951aa5c821425cc28aa
saudemaisfit-beep <saudemaisfit@gmail.com>
2026-06-25 08:50:42 +0100

    feat: replace contact hero image with Omatapalo-Contactos-web photo

--- src/app/contactos/page.tsx
+++ src/app/contactos/page.tsx
@@ -8,7 +8,7 @@ export default function ContactosPage() {
     <>
       <Nav />
       <main>
-        <PageHero title="Contactos" imgSrc="https://images.unsplash.com/photo-1423666639041-f56000c27a9a?w=1600&q=80" position="center top" imgOpacity={0.45} />
+        <PageHero title="Contactos" imgSrc="/Omatapalo-Contactos-web.jpg" position="center top" imgOpacity={0.45} />
         <Contact />
       </main>
       <Footer />
